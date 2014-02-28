92e6b8a936dcb6debe85639ef18a99b0eb7e66a6
Thorin <thorin.iv@gmail.com>
2014-02-28 14:24:55 +0000

    Changes routes from index, edit to edit, update

--- app/controllers/changeauthor_controller.rb
+++ app/controllers/changeauthor_controller.rb
@@ -3,23 +3,21 @@ class ChangeauthorController < ApplicationController
   menu_item :issues
   default_search_scope :issues
 
-  before_filter :find_issue
-  before_filter :authorize
-
-  def index
-    @users = @project.members.order(:firstname)
-    @issue_user = User.where(:id => @issue["author_id"]).first
-  end
+  before_filter :find_issue, :authorize
 
   def edit
+    @users = @project.members.order(:firstname)
+  end
+
+  def update
     old_author = @issue.author
     if @issue.update_attribute(:author_id, params[:authorid])
       flash[:notice] = l(:notice_successful_update)
       call_hook(:controller_redmine_changeauthor_edit_after_save, { :old_author => old_author.name, :new_author => @issue.author.name, :issue => @issue })
 
-      redirect_to :controller => "issues", :action => "show", :id => params[:id]
+      redirect_to :controller => 'issues', :action => 'show', :id => params[:id]
     else
-      redirect_to :controller => "changeauthor", :action => "edit", :id => params[:id]
+      redirect_to :controller => 'changeauthor', :action => 'edit', :id => params[:id]
     end
   end
 
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,4 @@
 RedmineApp::Application.routes.draw do
-  get '/changeauthor/index' => 'changeauthor#index'
-  post '/changeauthor/edit' => 'changeauthor#edit'
+  get '/changeauthor/:id/edit' => 'changeauthor#edit', :as => 'edit_changeauthor'
+  put '/changeauthor/:id/update' => 'changeauthor#update', :as => 'changeauthor'
 end
