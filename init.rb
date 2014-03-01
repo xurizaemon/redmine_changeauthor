83ab3b968539cc7f3e01cc5b990bacbe37e3b915
Thorin <thorin.iv@gmail.com>
2014-03-01 18:53:25 +0000

    Fixes permited actions

--- init.rb
+++ init.rb
@@ -7,12 +7,12 @@ Redmine::Plugin.register :redmine_changeauthor do
   name 'Redmine ChangeAuthor plugin'
   author 'Tom Stark @fragtom, Stephane Lapie @ASAHI Net'
   description 'Plugin for author change'
-  version '1.0.0' 
+  version '1.0.1'
   settings :default => {'redmine_changeauthor_log_setting' => 'no'}, :partial => 'settings/redmine_changeauthor_settings'
   requires_redmine :version_or_higher => '2.0.0'
-  
+
   project_module :issue_tracking do
-    permission :change_author, {:changeauthor => [:index, :edit]}, :require => :member
+    permission :change_author, {:changeauthor => [:update, :edit]}, :require => :member
   end
 end
 
