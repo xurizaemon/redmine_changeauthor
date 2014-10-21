f0bd9ca1cb7a22eb3ba4de6787837ae8ce30e633
Thorin <thorin.iv@gmail.com>
2014-10-21 12:57:34 +0100

    Refactored for compatibility

--- init.rb
+++ init.rb
@@ -1,18 +1,21 @@
 require 'redmine'
 
-require_dependency 'redmine_changeauthor/hooks'
-
 Redmine::Plugin.register :redmine_changeauthor do
-
   name 'Redmine ChangeAuthor plugin'
   author 'Tom Stark @fragtom, Stephane Lapie @ASAHI Net'
   description 'Plugin for author change'
   version '1.0.1'
-  settings :default => {'redmine_changeauthor_log_setting' => 'no'}, :partial => 'settings/redmine_changeauthor_settings'
   requires_redmine :version_or_higher => '2.0.0'
+
+  settings :default => {'redmine_changeauthor_log_setting' => 'no'}, :partial => 'settings/redmine_changeauthor_settings'
 
   project_module :issue_tracking do
     permission :change_author, {:changeauthor => [:update, :edit]}, :require => :member
   end
 end
 
+RedmineApp::Application.config.after_initialize do
+  require_dependency 'redmine_changeauthor/infectors'
+end
+
+require_dependency 'redmine_changeauthor/hooks'
--- lib/redmine_changeauthor/hooks.rb
+++ lib/redmine_changeauthor/hooks.rb
@@ -19,22 +19,4 @@ module RedmineChangeauthor
       journal.save
     end
   end
-
-  module IssuesHelperPatch
-    def self.included(base)
-      base.class_eval do
-        alias orig_show_detail_by_changeauthor show_detail
-        def show_detail(detail, no_html=false, options={})
-          if detail.property == 'attr' && detail.prop_key == 'author_id'
-            field = detail.prop_key.to_s.gsub(/\_id$/, "")
-            detail[:value] = find_name_by_reflection(field, detail.value) || detail.value
-            detail[:old_value] = find_name_by_reflection(field, detail.old_value) || detail.old_value
-          end
-          orig_show_detail_by_changeauthor(detail, no_html, options)
-        end
-      end
-    end
-  end
-
-  IssuesHelper.send(:include, IssuesHelperPatch) unless IssuesHelper.included_modules.include? IssuesHelperPatch
 end
